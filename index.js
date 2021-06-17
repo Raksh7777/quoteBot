83c5f6a7b136cfd8cc88254ff949c646ea4b4a1f
Rakshika Bawari <rakshika7777@gmail.com>
2021-06-17 20:28:34 +0530

    created server

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 const axios = require('axios');
+const http = require('http');
+const port = 3000;
+console.log(process.env.API_KEY);
+const server=http.createServer((req,res)=>{
 const options = {
     method:'GET',
     url:'https://quotes15.p.rapidapi.com/quotes/random/',
     headers: {
-    'x-rapidapi-key': 'fd59c21a7bmsh1aa397eef5bb6c6p11b6c5jsn37d544f0dcae',
+    'x-rapidapi-key': process.env.API_KEY,
     'x-rapidapi-host': 'quotes15.p.rapidapi.com'
     }
   };
-  
+ 
   axios.request(options).then(function (response) {
-	console.log(response.data.content);
+    var quote=response.data.content;
+    res.end(JSON.stringify({ quote})); 
+	//console.log(response.data.content);
 }).catch(function (error) {
 	console.error(error);
 });
+
+})
+server.listen(3000, () => {
+  console.log(`Server running at port ${port}`)
+})
